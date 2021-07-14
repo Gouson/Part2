4a1960a6462269273d2e93587d46046b2202d376
Gouson <gouson@qq.com>
2021-07-14 16:06:58 +0800

    response api

--- express-demo-4/app.js
+++ express-demo-4/app.js
@@ -1,44 +1,37 @@
-const {
-    response
-} = require('express')
 const express = require('express')
 const app = express()
-app.set('case sensitive routing', true) //带小写敏感，set要放在前面
 app.set('views', 'gouson')
 app.set('view engine', 'pug') //pug, ejs ...
-app.use(express.json()) //处理请求体json
 
-app.use(express.static('yyy')) //静态路径
-
-app.use(express.urlencoded()) //处理body x-www-form-urlencoded
-
-app.get('/style.css', (req, res, next) => {
-    res.send('style.css')
-})
-app.get('/STYLE.css', (req, res, next) => {
-    res.send('STYLE.css')
-})
 app.get('/test', (req, res, next) => {
-    res.render('test', {
-        pageTitle: '网页标题' //渲染 传入变量
+    // res.set('X-Gouson', 'yes')
+    // res.append('X-Gouson', 'yes2')
+    // res.status(401)
+    // res.send('hi')
+    // next()
+    res.format({
+        'text/plain': function () {
+            res.send('hey')
+        },
+
+        'text/html': function () {
+            res.send('<p>hey</p>')
+        },
+
+        'application/json': function () {
+            res.send({
+                message: 'hey'
+            })
+        },
+
+        default: function () {
+            // log the request and respond with 406
+            res.status(406).send('Not Acceptable')
+        }
     })
 })
-app.post('/test', (req, res, next) => {
-    res.send('post /test')
-})
-app.put('/test', (req, res, next) => {
-    res.send('put /test')
-})
-app.delete('/test', (req, res, next) => {
-    res.send('delete /test')
-})
-app.patch('/test', (req, res, next) => {
-    res.send('patch /test')
-})
-app.use((req, res, next) => {
-    console.log(req.body)
-    res.send('hi')
-    next()
+app.get('/gouson', (req, res, next) => {
+    res.send('gouson')
 })
 app.listen(3000, () => {
     console.log('3000')
