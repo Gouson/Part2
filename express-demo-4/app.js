8853000f2dbdd6fdcffd79aeaea935457930faef
Gouson <gouson@qq.com>
2021-07-14 17:01:26 +0800

    router api

--- express-demo-4/app.js
+++ express-demo-4/app.js
@@ -1,38 +1,7 @@
 const express = require('express')
 const app = express()
-app.set('views', 'gouson')
-app.set('view engine', 'pug') //pug, ejs ...
-
-app.get('/test', (req, res, next) => {
-    // res.set('X-Gouson', 'yes')
-    // res.append('X-Gouson', 'yes2')
-    // res.status(401)
-    // res.send('hi')
-    // next()
-    res.format({
-        'text/plain': function () {
-            res.send('hey')
-        },
-
-        'text/html': function () {
-            res.send('<p>hey</p>')
-        },
-
-        'application/json': function () {
-            res.send({
-                message: 'hey'
-            })
-        },
-
-        default: function () {
-            // log the request and respond with 406
-            res.status(406).send('Not Acceptable')
-        }
-    })
-})
-app.get('/gouson', (req, res, next) => {
-    res.send('gouson')
-})
+const user = require('./router/users')
+app.use('/users', user)
 app.listen(3000, () => {
     console.log('3000')
 })
---
+++ express-demo-4/router/users.js
@@ -0,0 +1,12 @@
+const express = require('express')
+var router = express.Router()
+router.get('/', (req, res, next) => {
+    res.send('/users')
+})
+router.get('/:id', (req, res, next) => {
+    res.send('/users/:id')
+})
+router.get('/:id/edit', (req, res, next) => {
+    res.send('/users/:id/edit')
+})
+module.exports = router
