3e118b437c900b31d202f4961f4312ade69d7b83
Gouson <gouson@qq.com>
2021-07-21 17:32:43 +0800

    Post

---
+++ nextjs-blog-1/src/entity/Post.ts
@@ -0,0 +1,12 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('posts')
+export class Post {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+    @Column('varchar')
+    title: string;
+    @Column('text')
+    content: string
+
+}
