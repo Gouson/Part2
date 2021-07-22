75d3329ec3beefd21f3e4608a7e494040600b250
Gouson <gouson@qq.com>
2021-07-22 18:00:59 +0800

    event init

---
+++ event-hub/src/index.ts
@@ -0,0 +1,5 @@
+class EventHub { 
+    on(){}
+    emit(){}
+}
+export default EventHub
---
+++ event-hub/test/index.ts
@@ -0,0 +1,6 @@
+import EventHub from '../src/index';
+
+console.log(EventHub)
+
+const eventHub = new EventHub()
+
