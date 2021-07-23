aa594dadbceb15e2533e9cad21d9212e57565280
Gouson <gouson@qq.com>
2021-07-23 15:18:29 +0800

    手写eventHub

--- event-hub/src/index.ts
+++ event-hub/src/index.ts
@@ -1,5 +1,29 @@
-class EventHub { 
-    on(){}
-    emit(){}
+class EventHub {
+    private cache: { [key: string]: Array<(data?: unknown) => void> } = {}
+    on(eventName: string, fn: (data?: unknown) => void) {
+        this.cache[eventName] = this.cache[eventName] || []
+        this.cache[eventName].push(fn)
+    }
+    emit(eventName: string, data?: unknown) {
+        (this.cache[eventName] || []).forEach(fn => fn(data))
+    }
+    off(eventName: string, fn: (data?: unknown) => void) {
+        this.cache[eventName] = this.cache[eventName] || []
+        let index = indexOf(this.cache[eventName], fn)
+        if (index === -1) return
+        this.cache[eventName].splice(index, 1)
+    }
 }
 export default EventHub
+/*  */
+function indexOf(array: Array<(data?: unknown) => void>, item: (data?: unknown) => void) {
+    if (array === undefined) return -1
+    let index = -1
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === item) {
+            index = i
+            break
+        }
+    }
+    return index
+}
--- event-hub/test/index.ts
+++ event-hub/test/index.ts
@@ -1,6 +1,42 @@
 import EventHub from '../src/index';
 
-console.log(EventHub)
+type TestCase = (message: string) => void
+const test1: TestCase = (message) => {
+    const eventHub = new EventHub()
+    console.assert(eventHub instanceof Object === true, "eventHub 是个对象")
+    console.log(message)
+}
 
-const eventHub = new EventHub()
+const test2: TestCase = (message) => {
+    const eventHub = new EventHub()
+    let called = false
+    eventHub.on("xxx", (data) => {
+        called = true
+        console.log("男篮", data)
+    })
+    eventHub.emit("xxx", "迈特凯")
+    setTimeout(() => {
+        console.assert(called)
+        console.log(message)
+    }, 1000)
+}
 
+const test3: TestCase = (message) => {
+    const eventHub = new EventHub()
+    let called = false
+    const fn = () => {
+        called = true
+        console.log("yyy")
+    }
+    eventHub.on("yyy", fn)
+    eventHub.off("yyy", fn)
+    eventHub.emit("yyy")
+    setTimeout(() => {
+        console.assert(called)
+        console.log(message)
+    }, 1000)
+}
+
+test1('EventHub可以创建对象')
+test2('on 之后 emit会触发on的回调函数')
+test3('off卸载监听')
