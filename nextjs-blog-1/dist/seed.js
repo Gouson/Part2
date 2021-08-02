126afa49cdb1a858fcba5000231d8d8113d56246
Gouson <gouson@qq.com>
2021-08-02 16:19:20 +0800

    创建三个数据表

--- nextjs-blog-1/dist/seed.js
+++ nextjs-blog-1/dist/seed.js
@@ -10,40 +10,15 @@ require("reflect-metadata");
 
 var _typeorm = require("typeorm");
 
-var _Post = require("./entity/Post");
-
 (0, _typeorm.createConnection)().then( /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(connection) {
-    var posts, dataArray, i;
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            _context.next = 2;
-            return connection.manager.find(_Post.Post);
-
-          case 2:
-            posts = _context.sent;
-
-            if (!(posts.length === 0)) {
-              _context.next = 8;
-              break;
-            }
-
-            dataArray = [];
-
-            for (i = 0; i < 10; i++) {
-              dataArray.push(new _Post.Post("Post ".concat(i), "\u7B2C".concat(i, "\u7BC7\u6587\u7AE0")));
-            }
-
-            _context.next = 8;
-            return connection.manager.save(dataArray);
-
-          case 8:
-            console.log('posts 数据填充了');
             connection.close();
 
-          case 10:
+          case 1:
           case "end":
             return _context.stop();
         }
